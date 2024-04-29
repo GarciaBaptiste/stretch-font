cf616fb23182b50ea05cba391877c5468332b6ac
GarciaBaptiste <baptiste1garcia@gmail.com>
2024-04-29 13:03:20 +0200

    add a word component who can split letters

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react'
 import './App.css'
 
+const Writer = ({children}) => {
+  if(typeof children === 'string') {
+    return (
+      <div className='word'>
+        {children.split('').map((char, index) => (
+          <div key={index} className={"letter "+char}></div>
+        ))}
+      </div>
+    )
+  }
+  return <div>{children}</div>
+}
+
 function App() {
 
   return (
     <>
+      <Writer>TEST</Writer>
     </>
   )
 }
